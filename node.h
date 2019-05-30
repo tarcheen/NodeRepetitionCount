1060678575f6e23edd6c8ed28b358d2478ea0b35
tarcheen <mirlohi@me.com>
2019-05-30 13:15:32 -0700

    optimize the code, so a future node with the same number does not have to check all the nodes again

--- node.h
+++ node.h
@@ -2,6 +2,7 @@
 # define FLAG
 
 #include <iostream>
+#include <set>
 
 using std::cout;
 using std::endl;
@@ -53,6 +54,11 @@ int count_repeats(node* n)
 	return count;
 }
 
+bool check_duplicate_nodes(node* forward, std::set<int>* nodeSet)
+{
+	return nodeSet->find(forward->data) != nodeSet->end();
+}
+
 node* most_repeat_node(node* n)
 {
 
@@ -64,15 +70,25 @@ node* most_repeat_node(node* n)
 
 	int to_beat = count_repeats(result);		// count the first node repeation
 	int temp_result;
+	std::set<int> numbers;						// store data anytime we go through a node
 
 	while (forward->next != nullptr)
 	{
+		
+		if (check_duplicate_nodes(forward, &numbers))			// check whether we already check the node with that data ( if forward use to point to data 2, we dont want to check for data 2 again for future
+		{
+			forward = forward->next;
+			continue;
+		}
+
 		temp_result = count_repeats(forward);	// count second node repetition 
 		if (temp_result > to_beat)				// if future nodes beat the current count
 		{
 			to_beat = temp_result;				// update the to beat count
 			result = forward;					// point the final answer to the current node the forward is pointing
 		}
+
+		numbers.insert(forward->data);
 		forward = forward->next;				// lets check next node
 	}
 
